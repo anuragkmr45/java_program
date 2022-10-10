c24e02b209a32db4b5020830ac5dc3f3e5f67039
anurag kumar <anurag45kmr@gmail.com>
2022-10-10 08:50:15 +0530

    cycle sort

--- array/length.java
+++
@@ -1,5 +0,0 @@
-package array;
-
-public class length {
-
-}
---
+++ array/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package array.sorting;
+
+public class InsertionSort {
+    static void ISort (int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] < array[j - 1]) {
+                    swap(array, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+}
