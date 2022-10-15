ec67d3a51cc98fde81bae28593639a2fe634bdd8
anurag kumar <anurag45kmr@gmail.com>
2022-10-15 07:39:17 +0530

    sum of digit

---
+++ recursion/recursion1/SumDigit.java
@@ -0,0 +1,10 @@
+package recursion.recursion1;
+
+public class SumDigit {
+    static int sum(int num) {
+        if ( num == 0 ) {
+            return 0;
+        }
+        return num % 10 + sum(num / 10);
+    }
+}
