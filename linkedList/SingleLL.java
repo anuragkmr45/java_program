3a106dd958c502f050e35d36866534406ab36d39
anurag kumar <anurag45kmr@gmail.com>
2022-11-18 09:12:56 +0530

    single linkedlist

---
+++ linkedList/SingleLL.java
@@ -0,0 +1,99 @@
+package linkedList;
+
+public class SingleLL {
+
+    Node head;
+
+    public class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data  = data;
+            this.next = null;
+        }
+    }
+
+    // add data at first
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = null;
+        head = newNode;
+    }
+
+    // add data at last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node lastNode = head;
+
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+
+        lastNode.next = newNode;
+    }
+
+    // delete element at first 
+    public void deleteFirst(int data) {
+
+        if (head == null) {
+            System.out.println("empty list");
+            return;
+        }
+
+        head = head.next;
+    }
+
+    // delete element at last
+    public void deleteLast(int data) {
+        Node lastNode = head.next;
+        Node seclastNode = head;
+
+        if (head == null) {
+            System.out.println("empty list");
+            return;
+        }
+
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+            seclastNode = seclastNode.next;
+        }
+
+        seclastNode.next = null;
+    }
+
+    // display list
+    public void display(){
+        if(head == null) {
+            System.out.println("empty list");
+        }
+
+        Node currNode = head;
+
+        while (currNode.next != null) {
+            System.out.print(currNode + " --> ");
+            currNode = currNode.next;
+        }
+
+        System.out.println("null");
+    }
+
+    // size of linkedlist
+    public void size() {
+        Node currNode = head;
+
+        int count = 0;
+        while (currNode.next != null) {
+            count = count + 1;
+        }
+
+        System.out.println("Size of linkedlist => " + count);
+    }
+}
