9ba774f74d1b1b48741738f04907c8f4787fe21d
anuragkmr45 <anuragkmr825@gmail.com>
2023-03-06 14:39:37 +0530

    find ASCII val of char

---
+++ strings/findASCIIVal.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class findASCIIVal {
+    static int fun(char element) {
+        return element + 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun('a'));
+    }
+}
