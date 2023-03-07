c0565069242136281dbde91dce1da8390bf7bf14
anuragkmr45 <anuragkmr825@gmail.com>
2023-03-07 13:24:47 +0530

    unique subset of a string

---
+++ recursion/level2/UniqueSubset.java
@@ -0,0 +1,41 @@
+// get all unique subset (no repetition of set) 
+// eg "abc" has subset "aaa, aa, a"
+// so we use Hashset
+
+package recursion.level2;
+
+import java.util.HashSet;
+
+public class UniqueSubset {
+    
+    static void uniqueSubset(String str) {
+        HashSet<String> set = new HashSet<>();
+        String res = new String();
+
+        helperUniqueSubset(str, res, set);
+    }
+
+    static void helperUniqueSubset(String orgStr, String resStr, HashSet<String> set) {
+        
+        if (orgStr.length() == 0) {
+            if (set.contains(resStr)) {
+                return;                
+            } else {
+                System.out.println(resStr);
+                set.add(resStr);
+                return;
+            }
+        }
+
+        char element = orgStr.charAt(0);
+
+        helperUniqueSubset(orgStr.substring(1), resStr + element, set);
+        helperUniqueSubset(orgStr.substring(1), resStr, set);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        uniqueSubset(str);
+    }
+
+}
