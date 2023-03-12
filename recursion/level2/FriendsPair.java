d91d02d7882a0538a8bdec21d725a91b6e5b92f5
anuragkmr45 <anuragkmr825@gmail.com>
2023-03-12 06:57:10 +0530

    friends pair problem

---
+++ recursion/level2/FriendsPair.java
@@ -0,0 +1,23 @@
+package recursion.level2;
+
+public class FriendsPair {
+    static int noOfWays(int n) {
+
+        if (n <= 0) {
+            return 0;
+        }
+        if (n <= 1) {
+            return 1;
+        }
+
+        int single = noOfWays(n - 1);
+        int pair = (n - 1) * noOfWays(n - 2);
+
+        return single + pair;
+    }
+
+    public static void main(String[] args) {
+        int num = -1;
+        System.out.println(noOfWays(num));
+    }
+}
