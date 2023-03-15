0a50f784e3da08b195a9afc4116b9e3da215e839
anuragkmr45 <anuragkmr825@gmail.com>
2023-03-15 08:10:17 +0530

    path maze can aslo move digonally

--- RecursionBacktracking/level2/CountPathMaze.java
+++ RecursionBacktracking/level2/CountPathMaze.java
@@ -5,6 +5,7 @@ package RecursionBacktracking.level2;
 import java.util.ArrayList;
 import java.util.List;
 
+// only move rigth or/and down
 public class CountPathMaze {
     static int initialCountPAth(int row, int col) {
         int rowCount = 0;
@@ -25,6 +26,7 @@ public class CountPathMaze {
         return right + down;
     }
 
+    // only move rigth or/and down
     static List<String> findPaths(int row, int col) {
         String path = "";
 
@@ -49,6 +51,27 @@ public class CountPathMaze {
         }
 
         return list;
+    }
+
+    // only move rigth or/and down and digonaly
+    static int initialCountPAthUpgrad(int row, int col) {
+        int rowCount = 0;
+        int colCount = 0;
+
+        return countPathUpgrad(row, col, rowCount, colCount);
+    }
+
+    static int countPathUpgrad(int row, int col, int rowCount, int colCount) {
+
+        if (rowCount == row - 1 || colCount == col - 1) {
+            return 1;
+        }
+
+        int right = countPath(row, col, rowCount + 1, colCount);
+        int down = countPath(row, col, rowCount, colCount + 1);
+        int diagonal = countPath(row, col, rowCount + 1, colCount + 1);
+
+        return right + down + diagonal;
     }
 
     public static void main(String[] args) {
