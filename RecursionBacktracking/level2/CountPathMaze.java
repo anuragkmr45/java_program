875035047676d0e1f8e2150f0703fca4dcf12c4f
anuragkmr45 <anuragkmr825@gmail.com>
2023-03-15 07:42:43 +0530

    maze path by names

--- RecursionBacktracking/level2/CountPathMaze.java
+++ RecursionBacktracking/level2/CountPathMaze.java
@@ -2,6 +2,9 @@
 
 package RecursionBacktracking.level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountPathMaze {
     static int initialCountPAth(int row, int col) {
         int rowCount = 0;
@@ -9,7 +12,7 @@ public class CountPathMaze {
 
         return countPath(row, col, rowCount, colCount);
     }
-    
+
     static int countPath(int row, int col, int rowCount, int colCount) {
 
         if (rowCount == row - 1 || colCount == col - 1) {
@@ -21,8 +24,35 @@ public class CountPathMaze {
 
         return right + down;
     }
-    
+
+    static List<String> findPaths(int row, int col) {
+        String path = "";
+
+        return helper(row, col, path);
+    }
+
+    static List<String> helper(int row, int col, String ans) {
+
+        if (row == 1 && col == 1) {
+            List<String> ansList = new ArrayList<>();
+            ansList.add(ans);
+            return ansList;
+        }
+
+        List<String> list = new ArrayList<>();
+
+        if (row > 1) {
+            list.addAll(helper(row - 1, col, ans + " Right "));
+        }
+        if (col > 1) {
+            list.addAll(helper(row, col - 1, ans + " Down "));
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
-        System.out.println(initialCountPAth(3, 3));
+        // System.out.println(initialCountPAth(3, 3));
+        System.out.println(findPaths(3, 3));
     }
 }
