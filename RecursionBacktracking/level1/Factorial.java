8612e5f552283d25ff719b265092402c5a772e97
anuragkmr45 <anuragkmr825@gmail.com>
2023-04-07 23:48:26 +0530

    factorial Ans

--- RecursionBacktracking/level1/Factorial.java
+++ RecursionBacktracking/level1/Factorial.java
@@ -1,10 +1,14 @@
 package RecursionBacktracking.level1;
 
 public class Factorial {
-    static int factorial (int num) {
+    static int factorial(int num) {
         if (num == 0) {
             return 1;
         }
         return num * factorial(num - 1);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
     }
 }
