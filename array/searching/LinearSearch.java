a1e10a95c128b4c4a4733f2e2b4c01f878028e16
anuragkmr45 <anuragkmr825@gmail.com>
2023-04-07 23:12:43 +0530

    result of LSearch

--- array/searching/LinearSearch.java
+++ array/searching/LinearSearch.java
@@ -19,4 +19,9 @@ public class LinearSearch {
         }
         return false;
     }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        System.out.println(LSearchPresent(arr, 6));
+    }
 }
