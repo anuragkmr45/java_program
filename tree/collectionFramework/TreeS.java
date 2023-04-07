2cb34e63eaf9efe5e2aa57f6728d07474a614f82
anuragkmr45 <anuragkmr825@gmail.com>
2023-04-08 01:35:12 +0530

    tree dir

---
+++ tree/collectionFramework/TreeS.java
@@ -0,0 +1,17 @@
+package tree.collectionFramework;
+
+import java.util.TreeSet;
+
+public class TreeS {
+    public static void main(String[] args) {
+        TreeSet<String> set = new TreeSet<String>();
+
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("d");
+        set.add("e");
+
+        System.out.println(set);
+    }
+}
