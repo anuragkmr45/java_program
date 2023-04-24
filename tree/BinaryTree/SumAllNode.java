0f585feadcf345b88eb1020716e7d501c17a3d99
anuragkmr45 <anuragkmr825@gmail.com>
2023-04-24 08:13:26 +0530

    sum of all nodes

---
+++ tree/BinaryTree/SumAllNode.java
@@ -0,0 +1,23 @@
+package tree.BinaryTree;
+
+public class SumAllNode {
+    public static int sumOfNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = sumOfNodes(root.left);
+        int right = sumOfNodes(root.right);
+
+        return left + right + root.data;
+    }
+
+    public static void main(String[] args) {
+        int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        CreateBT tree = new CreateBT();
+        Node root = tree.createTreeFromArr(nodes);
+
+        System.out.println(sumOfNodes(root));
+    }
+}
