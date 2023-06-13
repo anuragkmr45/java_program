e9f6b1d333b678882d65731d5eded15f2112bd01
anuragkmr45 <anuragkmr825@gmail.com>
2023-06-14 03:19:31 +0530

    queue question

---
+++ queue/Question/JosephusProblem.java
@@ -0,0 +1,55 @@
+package queue.Question;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class JosephusProblem {
+    public static int findLastPersonPosition(int n, int k) {
+        // Create a circular linked list with n nodes
+        Node head = createCircularList(n);
+
+        // Start eliminating people until only one person remains
+        while (head.next != head) {
+            // Move k-1 steps to reach the kth person
+            for (int i = 0; i < k - 1; i++) {
+                head = head.next;
+            }
+
+            // Eliminate the kth person
+            head.next = head.next.next;
+        }
+
+        // Return the position of the last remaining person
+        return head.data;
+    }
+
+    private static Node createCircularList(int n) {
+        Node head = new Node(1);
+        Node current = head;
+
+        // Create the circular linked list
+        for (int i = 2; i <= n; i++) {
+            current.next = new Node(i);
+            current = current.next;
+        }
+
+        // Connect the last node with the head to make it circular
+        current.next = head;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int n = 7; // Number of people
+        int k = 3; // Count to eliminate
+
+        int lastPerson = findLastPersonPosition(n, k);
+        System.out.println("Position of the last person: " + lastPerson);
+    }
+}
